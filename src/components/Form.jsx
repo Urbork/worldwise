b7d9307842397113996f86c1ba13a9a74e1dbbe0
Jonasz Jachym <jajonaszjj@gmail.com>
2025-03-04 16:26:10 +0100

    console log removed

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -48,7 +48,7 @@ function Form() {
             `${BASE_URL}?latitude=${lat}&longitude=${lng}`
           );
           const data = await res.json();
-          console.log(data);
+          // console.log(data);
 
           if (!data.countryCode)
             throw new Error(
